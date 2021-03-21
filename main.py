8cf065f50aae6aaba2660621449bb3b1af94c0ec
Michael Yudin <mihelitto@gmail.com>
2021-03-21 11:52:42 +0300

    Add creation of a short link via a function

--- main.py
+++ main.py
@@ -4,20 +4,33 @@ import json
 import requests
 import dotenv
 
-dotenv.load_dotenv()
-TOKEN = getenv("TOKEN")
 
-url = f"https://api-ssl.bitly.com/v4/user"
-url_bitlink = "https://api-ssl.bitly.com/v4/bitlinks"
-headers = {"Authorization": f"Bearer {TOKEN}",}
-data = json.dumps({"long_url": "https://github.com/Mihelitto"})
+def shorten_link(token, url):
+    url_bitlinks = "https://api-ssl.bitly.com/v4/bitlinks"
+    headers = {"Authorization": f"Bearer {token}", }
+    data = json.dumps({"long_url": url})
 
-response = requests.post(url_bitlink, headers=headers, data=data)
-response.raise_for_status()
+    response = requests.post(url_bitlinks, headers=headers, data=data)
+    response.raise_for_status()
 
-info = json.loads(response.text)
+    info = response.json()
+    print(info["link"])
 
-print(response.headers)
-pprint.pprint(response.json())
-pprint.pprint(info)
-print(info["link"])
+
+def main():
+    dotenv.load_dotenv()
+    TOKEN = getenv("TOKEN")
+    url = "https://github.com/Mihelitto"
+
+    try:
+        shorten_link(TOKEN, url)
+    except requests.exceptions.ConnectionError:
+        print("Сайт не отвечает.")
+    except requests.exceptions.HTTPError:
+        print("Страница не найдена.")
+
+
+url_user = f"https://api-ssl.bitly.com/v4/user"
+
+if __name__ == "__main__":
+    main()
