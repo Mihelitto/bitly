87569d429c2632a559d0ff51dc782f24e1d90183
Michael Yudin <mihelitto@gmail.com>
2021-03-21 12:52:03 +0300

    Add function to get the number of clicks

--- main.py
+++ main.py
@@ -17,13 +17,27 @@ def shorten_link(token, url):
     return info["link"]
 
 
+def count_clicks(token, bitlink):
+    url_clicks = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary"
+    headers = {"Authorization": f"Bearer {token}", }
+    params = {"unit": "day", "units": "-1"}
+
+    response = requests.get(url_clicks, headers=headers, params=params)
+    response.raise_for_status()
+
+    info = response.json()
+    return info["total_clicks"]
+
+
 def main():
     dotenv.load_dotenv()
     TOKEN = getenv("TOKEN")
-    url = input("Введите ссылку для сокращения: ").strip()
+    # url = input("Введите ссылку для сокращения: ").strip()
+    url = input("Введите ссылку, чтобы узнать количество кликов по ней: ").strip()
 
     try:
-        bitlink = shorten_link(TOKEN, url)
+        # bitlink = shorten_link(TOKEN, url)
+        total_clicks = count_clicks(TOKEN, url)
     except requests.exceptions.ConnectionError:
         print("Сайт не отвечает.")
         return
@@ -31,9 +45,9 @@ def main():
         print("Ошибка! Вы ввели неверную ссылку.")
         return
 
-
-    print(f"Теперь для доступа к {url} Вы можете воспользоваться следующей ссылкой:")
-    print(bitlink)
+    print(f"По ссылке {url} перешли {total_clicks} раз(а).")
+    # print(f"Теперь для доступа к {url} Вы можете воспользоваться следующей ссылкой:")
+    # print(bitlink)
 
 
 url_user = f"https://api-ssl.bitly.com/v4/user"
