544d8c1e90df53bc4c66d7db4a9e855904087228
Michael Yudin <mihelitto@gmail.com>
2021-03-21 11:27:12 +0300

    Create short link

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ dotenv.load_dotenv()
 TOKEN = getenv("TOKEN")
 
 url = f"https://api-ssl.bitly.com/v4/user"
+url_bitlink = "https://api-ssl.bitly.com/v4/bitlinks"
 headers = {"Authorization": f"Bearer {TOKEN}",}
+data = json.dumps({"long_url": "https://github.com/Mihelitto"})
 
-response = requests.get(url, headers=headers)
+response = requests.post(url_bitlink, headers=headers, data=data)
+response.raise_for_status()
 
-user_info = json.loads(response.text)
+info = json.loads(response.text)
 
 print(response.headers)
-pprint.pprint(user_info)
+pprint.pprint(response.json())
+pprint.pprint(info)
+print(info["link"])
